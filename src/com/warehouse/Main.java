4cc28ae6f7ea01aa2c0f497469ed43a5111920f1
Gouri Yadav <gouriyadav2009@gmail.com>
2025-10-22 22:46:20 +0530

    Product Class committed

---
+++ src/com/warehouse/Main.java
@@ -0,0 +1,10 @@
+package com.warehouse;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/warehouse/Product.java
@@ -0,0 +1,59 @@
+package com.warehouse;
+
+public class Product {
+
+	private final String id;
+	private final String name;
+	private int quantity;
+	private final int reorderThreshold;
+	
+	public Product(String id,String name,int reorderThreshold) {
+		this.id=id;
+		this.name=name;
+		this.reorderThreshold=reorderThreshold;
+		this.quantity=0;
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public int getReorderThreshold() {
+		return reorderThreshold;
+	}
+
+	
+	public void addStock(int amount) {
+		if(amount>0) {
+		quantity=quantity+amount;
+		}
+	}
+	
+	public void reduceStock(int amount) {
+		if(amount<=0 && amount>quantity) {
+		throw new IllegalArgumentException("Invalid amount or Insufficient stock");
+			
+		}
+		else {
+		quantity=quantity-amount;
+		}
+		
+	}
+	@Override
+	public String toString() {
+		return name+"[Quantity : "+quantity+ "]";
+	}
+
+	
+	
+	
+}
